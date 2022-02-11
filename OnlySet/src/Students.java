83fc27e9029bfdc8a5c1a1a92d26b72a104df693
duongmoe174 <duongmoe174@gmail.com>
2022-02-11 15:26:22 +0700

    up

---
+++ OnlySet/src/Students.java
@@ -0,0 +1,24 @@
+public class Students {
+    private String name = "John";
+    private String classes = "C02";
+
+    public Students() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClasses() {
+        return classes;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+
+}
---
+++ OnlySet/src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        Students st1 = new Students();
+        st1.setName("DuongDZ");
+        st1.setClasses("C00000000000000000000000000002");
+        System.out.println(st1.getName());
+        System.out.println(st1.getClasses());
+    }
+}
