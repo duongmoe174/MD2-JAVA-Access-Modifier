2ca950b2eb64129bc0d31f0f6227537f10e55d9a
duongmoe174 <duongmoe174@gmail.com>
2022-02-14 09:43:35 +0700

    update

--- ManagerBook/src/Demo.java
+++ ManagerBook/src/Demo.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Demo {
@@ -9,14 +10,17 @@ public class Demo {
         listBook[3] = new Book("May", "Duong", 50000, 40, 4);
         listBook[4] = new Book("Tui", "Duong", 60000, 70, 2);
 
-        int sumQuantity = 0;
-        int sumWeight = 0;
-        for (int i = 0; i < listBook.length; i++) {
-            sumQuantity += listBook[i].getNumber();
-            sumWeight += listBook[i].getWeight();
-        }
-        System.out.println("Tổng số lượng sách là: " + sumQuantity);
-        System.out.println("Tổng số lượng cân là: " + sumWeight);
+//        int sumQuantity = 0;
+//        int sumWeight = 0;
+//        for (int i = 0; i < listBook.length; i++) {
+//            sumQuantity += listBook[i].getNumber();
+//            sumWeight += listBook[i].getWeight();
+//        }
+//        System.out.println("Tổng số lượng sách là: " + sumQuantity);
+//        System.out.println("Tổng số lượng cân là: " + sumWeight);
+
+        System.out.println("Tổng tiền: " + totalPrice(listBook));
+        System.out.println("Tổng khối lượng: " + totalWeight(listBook));
 
         System.out.println("nhập tên sách cần tìm kiếm: ");
         Scanner inputName = new Scanner(System.in);
@@ -31,11 +35,30 @@ public class Demo {
         findBook(listBook, name);
     }
     public static void findBook (Book[] arr, String name) {
+        String result = "không tìm thấy";
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i].getName().equals(name)) {
+            if (Objects.equals(arr[i].getName(), name)) {
                 System.out.println("Tìm thấy sách r nè!");
                 break;
             }
+            if (i == arr.length - 1) {
+                System.out.println(result);
+            }
         }
+    }
+    public static int totalPrice (Book[] arr) {
+        int sumPrice = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sumPrice += arr[i].getTotalPrice();
+        }
+        return sumPrice;
+    }
+
+    public static int totalWeight (Book [] arr) {
+        int sumWeight = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sumWeight += arr[i].getWeight();
+        }
+        return sumWeight;
     }
 }
